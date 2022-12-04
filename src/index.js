87ec01955f87beeba148a1214fe1181ce3c2a529
Nathan Kwon <nathan.kwon.8.18@gmail.com>
2022-12-03 19:51:34 -0800

    reverting to debug import bugs

--- src/index.js
+++ src/index.js
@@ -9,11 +9,120 @@
 
   
 // }
-import KeyDownHandler from './scripts/key_down_handler.js'
-
 window.addEventListener('load', function() {
-  
-  const keyDownInp = new KeyDownHandler
+  const canvas = document.querySelector('canvas');
+  const context = canvas.getContext('2d');
+  context.fillStyle = '#95c88c'
+  context.fillRect(0, 0, 1920, 1080)
+
+
+class keyDownHandler {
+    constructor(){
+    this.keys = [];
+
+    
+    window.addEventListener('keydown', function(e){
+        e.preventDefault()
+        e.stopImmediatePropagation()
+        
+        const audioContext = new AudioContext()
+        e.stopImmediatePropagation()
+        const audioElement = document.querySelector('#sound-1')
+        console.log(audioElement instanceof HTMLAudioElement)
+        const track = audioContext.createMediaElementSource(audioElement);
+        console.log('im dying')
+        track.connect(audioContext.destination)
+
+        console.log(e.code)
+        switch(e.code) {
+        case "KeyQ":
+       
+        audioElement.play()
+        break;
+        
+        case "KeyW":
+        break;
+
+        case "KeyE":
+        break;
+
+        case "KeyR":
+        break;
+
+        case "KeyT":
+        break;
+
+        case "KeyY":
+        break;
+
+        case "KeyU":
+        break;
+
+        case "KeyI":
+        break;
+
+        case "KeyO":
+        break;
+
+        case "KeyP":
+        break;
+
+        case "KeyA":
+        break;
+
+        case "KeyS":
+        break;
+
+        case "KeyD":
+        break;
+
+        case "KeyF":
+        break;
+
+        case "KeyG":
+        break;
+
+        case "KeyH":
+        break;
+
+        case "KeyJ":
+        break;
+
+        case "KeyK":
+        break;
+
+        case "KeyL":
+        break;
+
+        case "KeyZ":
+        break;
+
+        case "KeyX":
+        break;
+
+        case "KeyC":
+        break;
+
+        case "KeyV":
+        break;
+
+        case "KeyB":
+        break;
+
+        case "KeyN":
+        break;
+
+        case "KeyM":
+        break;
+    }});
+}};
+
+
+
+
+
+
+const keyDownInput = new keyDownHandler
 
 }); //outer Window Event Listener bounds
       
